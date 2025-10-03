940f69bf8cb1a18aed0c49d2661645574d20e999
Ella Dunbar <git@elladunbar.com>
2025-10-03 18:31:22 -0400

    Convert to python and use command-line arguments

--- aliases.py
+++ aliases.py
@@ -1,8 +1,13 @@
-import hy
+#!/usr/bin/env uv run
+
 import json
 import os
-from dotenv import load_dotenv
+import sys
+
 import requests
+from dotenv import load_dotenv
+
+
 HEADERS = {'Content-Type': 'application/json'}
 load_dotenv()
 MIGADU_DOMAIN = os.getenv('MIGADU_DOMAIN')
@@ -12,13 +17,16 @@ URL = 'https://api.migadu.com/v1/domains/' + MIGADU_DOMAIN + '/aliases'
 AUTH = (MIGADU_USER, MIGADU_KEY)
 data = {'destinations': 'me@' + MIGADU_DOMAIN}
 
+
 def print_as_json(jsonable):
     return print(json.dumps(jsonable, indent=2))
-_hy_anon_var_1 = None
-with open('needed_aliases.txt', 'r') as f:
-    needed_aliases = tuple(map(lambda l: l.strip(), f.readlines()))
-    _hy_anon_var_1 = None
-for needed_alias in needed_aliases:
-    data['local_part'] = needed_alias
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        exit("Must provide the name of one alias to be created!")
+
+    data['local_part'] = sys.argv[1]
     response = requests.post(URL, auth=AUTH, json=data, headers=HEADERS)
-    print(needed_alias) if response.status_code != 200 else None
+    if response.status_code != 200:
+        print(f"Alias creation failed for {sys.argv[1]}")
